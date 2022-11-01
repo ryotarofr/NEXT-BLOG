659190f69a7cbae0df2a2f617d0831e5b9af08d0
ryotarofr <81869509+ryotarofr@users.noreply.github.com>
2022-11-01 21:04:41 +0900

    4

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,6 @@
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-<<<<<<< HEAD
-  return (
-    <div>
-      
-    </div>
-  );
-=======
   return <div></div>;
->>>>>>> 1c5b4ea69123ec436793a8ed7b23b2750b6b7411
+
 }
