1c3bcfd615b8f35a9dfc31e1c6ddd150b2739926
ryotarofr <ryoryo.fr0608@icloud.com>
2022-11-04 05:19:20 +0900

    create User Post auth and connect mongob

---
+++ backend/models/User.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      min: 3,
+      mat: 25,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 50,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    followings: {
+      type: Array,
+      default: [],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    desc: {
+      type: String,
+      max: 70,
+    },
+  },
+
+  // User情報の作成日時を表示
+  { timestamps: true }
+);
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,35 @@
 const express = require("express");
 const app = express();
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
+const postRoutes = require("./routes/posts");
 const PORT = 4000;
+const mongoose = require("mongoose");
+
+// closed mongodb url
+require("dotenv").config();
+
+// connect database
+mongoose
+  .connect(process.env.MONGOURL)
+  .then(() => {
+    console.log("connect mongodb...");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+//ミドルウェア
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
 
 app.get("/", (req, res) => {
   res.send("hello");
 });
+
+// app.get("/users", (req, res) => {
+//   res.send("users");
+// });
 
 app.listen(PORT, () => console.log("loading server...."));
